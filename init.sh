b3c7d9ed6360d4fa8b6c8e9b4a0f2c1339fac9e5
Jason Smith <jasssmith@redhat.com>
2017-08-02 14:29:42 -0400

    reduce the amount of time to restart the container, and require build only once.

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-docker exec -it patternkit composer install
 docker exec -it patternkit rm -rf node_modules
-docker exec -it patternkit npm install -g grunt grunt-cli
 docker exec -it patternkit npm install
 docker exec -it patternkit rm -rf bower_components
 docker exec -it patternkit bower install --allow-root
